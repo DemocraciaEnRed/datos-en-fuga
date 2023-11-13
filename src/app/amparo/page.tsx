fa2276bce0dd00f4959ebde3a175d3ed65e7acea
NicoC0 <nicoconfaa@gmail.com>
2023-11-13 10:20:00 -0300

    styles of the desktop version only

--- src/app/amparo/page.tsx
+++ src/app/amparo/page.tsx
@@ -16,8 +16,71 @@ const Amparo = () => {
         <h2 className='text-center text-5xl font-nippo font-bold max-[376px]:text-2xl'>
           PRESENTAMOS UN AMPARO PARA PROTEGER NUESTROS DATOS PERSONALES Y NUESTRA DEMOCRACIA
         </h2>
-        <div className='my-10'>
-          <h1 className='text-5xl text-center'>acá va el comic</h1>
+        <div className='my-10 flex flex-col justify-center items-center'>
+          <Image 
+            alt="amparo"
+            src="/amparo/desktop/desktop1.png"
+            width={1600}
+            height={621}
+          />
+          <Image 
+          alt="amparo"
+          src="/amparo/desktop/desktop2.png"
+          width={1600}
+          height={621}
+        />
+        </div>
+        <p className='text-5xl font-extrabold font-nippo text-center'>¿Y QUÉ ES LO QUE PUEDE PASAR MIENTRAS SIGA VIGENTE?</p>
+        <div className='my-7 flex flex-row flex-wrap gap-2 w-[80%] items-center justify-center mx-auto'>
+          <Image 
+            alt="amparo"
+            src="/amparo/desktop/desktop3.png"
+            width={535}
+            height={456}
+          />
+          <Image 
+            alt="amparo"
+            src="/amparo/desktop/desktop4.png"
+            width={535}
+            height={456}
+          />
+          <Image 
+            alt="amparo"
+            src="/amparo/desktop/desktop5.png"
+            width={535}
+            height={456}
+          />
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+        <Image 
+            alt="amparo"
+            src="/amparo/desktop/desktop6.png"
+            width={1600}
+            height={420}
+            className='hidden md:block'
+          />
+          <Image 
+            alt="amparo"
+            src="/amparo/mobile/mobile6.png"
+            width={1600}
+            height={420}
+            className='block md:hidden'
+          />
+        </div>
+        <p className='text-5xl font-extrabold font-nippo mx-auto text-center w-[65%] my-6'>PERO, ADEMÁS, EL USO DE DATOS PERSONALES TAMBIÉN AMENAZA LA DEMOCRACIA Y TIENE GRANDES ANTECEDENTES…</p>
+        <div className='flex flex-col justify-center items-center gap-2'>
+          <Image 
+            alt="amparo"
+            src="/amparo/desktop/desktop8.png"
+            width={1600}
+            height={420}
+          />
+          <Image 
+            alt="amparo"
+            src="/amparo/desktop/desktop9.png"
+            width={1600}
+            height={420}
+          />
         </div>
       </div>
       <div className='py-[8vh] bg-[#2D2D2D]'>
