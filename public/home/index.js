774e98e8355a936d5d2614060edbb9690e6f4fdb
Pano Valdez <marianovaldez92@protonmail.com>
2023-12-12 14:12:33 -0300

    fix import relative

--- public/home/index.js
+++ public/home/index.js
@@ -1,6 +1,6 @@
-import desconocido from "/home/desconocido.png" 
-import databreach from "/home/databreach.png" 
-import ransomware from "/home/ransomware.png" 
-import incidente from "/home/incidente.png" 
+import desconocido from "./desconocido.png" 
+import databreach from "./databreach.png" 
+import ransomware from "./ransomware.png" 
+import incidente from "./incidente.png" 
 
 export const flourishImages = { desconocido, databreach, ransomware, incidente }
